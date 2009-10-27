71b88c51fefbabe527bdb6338cdbcd3dd361fcc3
Mikhail S. Pobolovets <styx.mp@gmail.com>
2009-10-27 16:03:40 +0200

    Main file has been cutted into Core.hs and gtc.hs

---
+++ Core.hs
@@ -0,0 +1,27 @@
+module Core
+    (
+      do_trans
+    , do_trans_each_word
+    )
+where
+
+import Data.List
+import Data.Maybe
+import Network.URL
+import Network.HTTP
+
+trans_url :: String -> String -> String -> String
+trans_url sl tl text =
+    exportURL $ foldl add_param base_url [("text", text), ("sl", sl), ("tl", tl)]
+        where base_url = fromJust $ importURL "http://translate.google.com/translate_a/t?client=t&pc=0&oc=1&hl=en&ie=UTF-8&oe=UTF-8"
+
+-- TODO: Need to add JSON parsing
+do_trans :: String -> String -> String -> IO String
+do_trans sl tl str =
+  do result <- simpleHTTP (getRequest $ trans_url sl tl str) >>= getResponseBody
+     return result
+
+do_trans_each_word :: String -> String -> String -> IO String
+do_trans_each_word sl tl str = undefined
+--  do result <- simpleHTTP (getRequest $ trans_url sl tl word) >>= getResponseBody
+--     return result
--- gtc.hs
+++ gtc.hs
@@ -1,25 +1,8 @@
 
+import Core
 import Data.List
-import Data.Maybe
-import Network.URL
-import Network.HTTP
-import Control.Monad
 import System.Exit
-import System.Environment
 import qualified System.Environment.UTF8 as U
-
-translation_url :: String -> String -> String -> String
-
-translation_url sl tl text =
-    exportURL $ foldl add_param base_url [("text", text), ("sl", sl), ("tl", tl)]
-        where base_url = fromJust $ importURL "http://translate.google.com/translate_a/t?client=t&pc=0&oc=1&hl=en&ie=UTF-8&oe=UTF-8"
-
--- TODO: Need to add JSON parsing
-do_translation :: String -> String -> String -> IO String
-do_translation sl tl word =
-  do result <- simpleHTTP (getRequest $ translation_url sl tl word) >>= getResponseBody
-     putStrLn $ translation_url "en" "ru" word
-     return result
 
 usage :: IO ()
 usage =
@@ -34,6 +17,6 @@ main =
      -- TODO: Need to detecet terminal encoding and make convertions
      args <- U.getArgs
      case args of
-        from:to:rest -> do_translation from to (intercalate " " rest) >>= putStrLn
+        from:to:rest -> do_trans from to (intercalate " " rest) >>= putStrLn
         _        -> usage
      exitWith $ ExitSuccess
