4e9067a339a548da7b8082b661d38cb0dd84ceb6
Sergei Trofimovich <slyfox@inbox.ru>
2010-03-11 10:04:54 +0200

    gtg: fixed output encoding handling for ghc-6.12 (+fixed some trivial warnings)
    Signed-off-by: Sergei Trofimovich <slyfox@inbox.ru>

--- Gtg/gtg.hs
+++ Gtg/gtg.hs
@@ -12,7 +12,7 @@ import Paths_gt_tools(getDataDir)
 
 main :: IO()
 main = do
-    initGUI
+    _ <- initGUI
     data_dir  <- getDataDir
     Just xml  <- xmlNew $ data_dir ++ "/Gtg/main.glade"
 
@@ -25,17 +25,17 @@ main = do
     cmb_dest_lang   <- xmlGetWidget xml castToComboBox  "cmb_dest_lang"
     cmb_source_lang <- xmlGetWidget xml castToComboBox  "cmb_source_lang"
 
-    comboBoxSetModelText cmb_dest_lang
-    comboBoxSetModelText cmb_source_lang
+    _ <- comboBoxSetModelText cmb_dest_lang
+    _ <- comboBoxSetModelText cmb_source_lang
 
     mapM_ (comboBoxAppendText cmb_dest_lang  ) (tail langs_descrs)
     mapM_ (comboBoxAppendText cmb_source_lang) langs_descrs
 
     -- Actions
-    onClicked       btn_close      (widgetDestroy window)
-    onClicked       btn_translate  (trans_click ent_trans_text lbl_translation cmb_source_lang cmb_dest_lang)
-    onEntryActivate ent_trans_text (trans_click ent_trans_text lbl_translation cmb_source_lang cmb_dest_lang)
-    onDestroy       window         mainQuit
+    _ <- onClicked       btn_close      (widgetDestroy window)
+    _ <- onClicked       btn_translate  (trans_click ent_trans_text lbl_translation cmb_source_lang cmb_dest_lang)
+    _ <- onEntryActivate ent_trans_text (trans_click ent_trans_text lbl_translation cmb_source_lang cmb_dest_lang)
+    _ <- onDestroy       window         mainQuit
 
     -- Let's go
     widgetShowAll window
@@ -57,4 +57,4 @@ trans_click ent lbl cmb_src cmb_dest = do
     src <- get_src_lang cmb_src
     dst <- get_dest_lang cmb_dest
     translated_text <- do_trans src dst text
-    set lbl [ labelText := decodeString translated_text ]
+    set lbl [ labelText := translated_text ]
