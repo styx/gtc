5a230ebc16967699f2c842f3a7804167aa799b38
Mikhail S. Pobolovets <styx.mp@gmail.com>
2010-03-12 16:59:39 +0200

    Interactive mode has been introduced.
    Use -i and --interactive commandline keys.

--- Gtc/gtc.hs
+++ Gtc/gtc.hs
@@ -5,37 +5,81 @@ module Main (
 
 import Gt.Core
 import Gt.Langs
-import Data.List (intercalate)
+import Control.Monad
+import System.IO.Error
 import System.Exit
 import qualified System.Environment.UTF8 as U
 
-usage :: IO ()
+usage :: IO()
 usage =
   do
-     mapM_ putStrLn [ "    Usage: 'fromLang' 'toLang' 'text'"
-                    , "    Lang is 2 sumbols code, i.e. be, en, ru.\n"
-                    , "        --help or -h - to see this help."
-                    , "        --list or -l - list of available languages.\n"
+     mapM_ putStrLn [ "    Usage:"
+                    , "         Standart mode: gtc 'fromLang' 'toLang' 'text'"
+                    , "         Interactive mode: gtc -i 'fromLang' 'toLang'"
+                    , "             Where 'Lang' is 2 sumbols code, i.e. be, en, ru.\n"
+                    , "    Commandline keys:"
+                    , "        --interactive or -i - interactive mode."
+                    , "        --help        or -h - to see this help."
+                    , "        --list        or -l - list of available languages.\n"
                     ]
      exitWith $ ExitFailure 1
 
-langs_list :: IO ()
+langs_list :: IO()
 langs_list = putStrLn $ "\nList of available languages:\n\n" ++
              (concat $ zipWith (\l d -> "    " ++ l ++ " - " ++ d ++ "\n") langs langs_descrs)
 
-main :: IO ()
+main :: IO()
 main =
   do
-     -- TODO: Need to detecet terminal encoding and make convertions
-     args <- U.getArgs
-     case args of
+    -- TODO: Need to detecet terminal encoding and make convertions
+    args <- U.getArgs
+    case args of
 
-        ["-l"]       -> langs_list
-        ["-h"]       -> usage
+        "-i":rest -> interactiveLoop rest
+        ["-l"]    -> langs_list
+        ["-h"]    -> usage
 
-        ["--list"]   -> langs_list
-        ["--help"]   -> usage
+        "--interactive":rest -> interactiveLoop rest
+        ["--list"]           -> langs_list
+        ["--help"]           -> usage
 
-        from:to:rest -> do_trans from to (intercalate " " rest) >>= putStrLn
+
+        -- Old variant with 'single line' processing
+        -- from:to:rest -> do_trans from to (intercalate " " rest) >>= putStrLn
+
+        from:to:rest -> mapM_ (>>= putStrLn) (map (do_trans from to) rest)
         _            -> usage
-     exitWith ExitSuccess
+
+    exitWith ExitSuccess
+
+-- Interactive mode processor
+-- TODO:
+-- 1) It shoud be possible to memoize: interactiveLoop' from to
+-- 2) Need to remove explicit recursion
+-- 3) Need to skip blank lines (including lines consist of white spaces only)
+-- 4) Split results with '-------------------' ???
+-- 5) Remove some redunt carriage returns to make results looks prettie
+interactiveLoop :: [String] -> IO()
+interactiveLoop params =
+  do
+    case params of
+        from:to:_ -> interactiveLoop' from to
+        _         -> usage
+
+interactiveLoop' :: Lang -> Lang -> IO()
+interactiveLoop' from to =
+  do
+    maybeLine <- getLine'
+    case maybeLine of
+        Nothing   -> return ()
+        Just line -> do
+                       do_trans from to line >>= putStrLn
+                       interactiveLoop' from to
+
+getLine' :: IO(Maybe String)
+getLine' =
+  do
+    (liftM Just getLine ) `catch` eofHandler where
+    eofHandler e = if isEOFError e
+                   then return Nothing
+                   else ioError e
