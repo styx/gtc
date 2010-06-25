48f5ee1235c50e71de01191ee27edef79c608724
Mikhail S. Pobolovets <styx.mp@gmail.com>
2010-06-25 09:06:45 +0300

    FIX: small change in the sight of Google bug fixing
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- Gt/Net.hs
+++ Gt/Net.hs
@@ -16,7 +16,7 @@ import Network.HTTP
 trans_url :: Lang -> Lang -> String -> String
 trans_url sl tl text =
     exportURL $ foldl add_param base_url [("text", text), ("sl", sl), ("tl", tl)]
-        where base_url = fromJust $ importURL "http://translate.google.com/translate_a/t?client=t&pc=0&oc=1&hl=en&ie=UTF-8&oe=UTF-8"
+        where base_url = fromJust $ importURL "http://translate.google.com/translate_a/t?client=j&pc=0&oc=1&hl=en&ie=UTF-8&oe=UTF-8"
 
 get_resp :: Lang -> Lang -> String -> IO String
 get_resp sl tl str =
